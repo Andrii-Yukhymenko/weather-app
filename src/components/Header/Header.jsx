7fb27f812c7bf31ae6fd6d2e7de74dd1fb5ab660
Andrii Yukhymenko <andriy111224@gmail.com>
2022-01-31 19:34:20 +0200

    Изменение темы

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import headerLogo from "../../images/Headerlogo.svg";
 import themeSwitcher from "../../images/ThemeSwitch.svg";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
@@ -11,15 +11,27 @@ function Header() {
     e.target[0].value = "";
   };
 
+  let LSTheme = localStorage.getItem("theme");
+  const [theme, setTheme] = useState(LSTheme ? LSTheme : "light");
+
+  const toggleTheme = () => {
+    setTheme(LSTheme === "light" ? "dark" : "light");
+    localStorage.setItem("theme", theme === "light" ? "dark" : "light");
+  };
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  });
+
   return (
     <header className="header">
       <div className="header__container container">
-        <Link className="header-logo" to='/'>
+        <Link className="header-logo" to="/">
           <img src={headerLogo} alt="logo icon" className="header-logo__icon" />
           <h1 className="header-logo__title">REACT WEATHER</h1>
         </Link>
         <div className="header-controls">
           <img
+            onClick={toggleTheme}
             src={themeSwitcher}
             alt="switch theme"
             className="header-controls__theme-switcher"
